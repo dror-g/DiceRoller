0e402510a5c731cfd7328e11766e8f0d7b1d4c6a
Dror G <nihil75@gmail.com>
2023-11-04 12:47:19 +0000

    added comments

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ def dice(Dmin=1, Dmax=6):
    return roll
 
 
+## Dice sized according to user-specified choices
 def decision(choices):
     Dmax = len(choices)
     return dice(Dmax=Dmax)
 
 
 def main():
-    # get cli arguments
+    # get arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--choices", nargs="+", default=None, help="Comma-separated list of choices to choose from")
     args = parser.parse_args()
     
     if args.choices:
+        # Roll dice, return the chosen choice
+        # We subtract 1 from decision because args.choices starts at 0
         chosen = args.choices[decision(args.choices) - 1]
         print(f"chosen choice is {chosen}")
     else:
